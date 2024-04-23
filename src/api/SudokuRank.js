8ee110e638880db3866590b9c6ab6247b976f887
JSG-8579 <rkfntmdrhks@naver.com>
2024-04-23 17:46:52 +0900

    반응형빼고 완

--- src/api/SudokuRank.js
+++ src/api/SudokuRank.js
@@ -6,13 +6,13 @@ import axios from 'axios';
 export const fetch= {
 
     get:async ()=>{
-        const res = await axios('https://port-0-sql-server-o0ynn2alrk7bxxu.sel5.cloudtype.app/sudoku')
+        const res = await axios(process.env.VERCEL)
         
         return res.data
     },
 
     post:async (value)=>{
-        const res = await axios.post('https://port-0-sql-server-o0ynn2alrk7bxxu.sel5.cloudtype.app/sudoku',value)
+        const res = await axios.post(process.env.VERCEL,value)
         
         return res.data
     },
