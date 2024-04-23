9f6d6e0b5eb2e16c0c460b74c045ea8998d157ba
JSG-8579 <rkfntmdrhks@naver.com>
2024-04-23 17:17:57 +0900

    서버배포

--- src/api/SudokuRank.js
+++ src/api/SudokuRank.js
@@ -6,13 +6,13 @@ import axios from 'axios';
 export const fetch= {
 
     get:async ()=>{
-        const res = await axios('http://localhost:3000/sudoku')
+        const res = await axios('https://port-0-sql-server-o0ynn2alrk7bxxu.sel5.cloudtype.app/sudoku')
         
         return res.data
     },
 
     post:async (value)=>{
-        const res = await axios.post('http://localhost:3000/sudoku',value)
+        const res = await axios.post('https://port-0-sql-server-o0ynn2alrk7bxxu.sel5.cloudtype.app/sudoku',value)
         
         return res.data
     },
