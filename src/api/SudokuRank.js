06303f6d3154cebe1c8305cef64955d7619eefbd
JSG-8579 <rkfntmdrhks@naver.com>
2024-04-25 10:25:24 +0900

    22

--- src/api/SudokuRank.js
+++ src/api/SudokuRank.js
@@ -6,13 +6,13 @@ import axios from 'axios';
 export const fetch= {
 
     get:async ()=>{
-        const res = await axios(process.env.VERCEL)
+        const res = await axios(process.env.VUE_APP_VERCEL)
         
         return res.data
     },
 
     post:async (value)=>{
-        const res = await axios.post(process.env.VERCEL,value)
+        const res = await axios.post(process.env.VUE_APP_VERCEL,value)
         
         return res.data
     },
